2fed08c6becb04a2eacc14c86177a8779967463b
aryanagra01 <aryanagra01@users.noreply.github.com>
2026-02-23 15:06:19 +0000

    deploy: 00187fe9a2c7cadf7c9b509ace29aa2d59f6b63b

---
+++ _next/static/K2w_TiWMyAxwWCTpH-c7Z/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/cleantext//_next/:path+",
+        "destination": "/cleantext/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
